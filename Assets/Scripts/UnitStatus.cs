a1793721d991a3ccb26c14e7c060d6c33e4c4938
CazNm <namaga132@gmail.com>
2019-03-12 19:16:32 +0900

    unity files

---
+++ Assets/Scripts/UnitStatus.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitStatus : MonoBehaviour
+{
+
+    public int M_health;
+    public int C_health;
+    public int C_shield;
+    public int attack;
+    public int attack_mode;
+    public int movement;
+    public int movement_mode;
+    public int hand_limit;
+    public GameObject[] stat_ui_list;
+
+    private int render_int;
+    private float test_position;
+    private Transform Health;
+    Transform King_position;
+
+    Vector3 s_position;
+    Vector2 King_vector;
+    
+    GameObject Health_bar;
+    GameObject Health_bar_R;
+    GameObject Health_contents;
+    GameObject Health_contents_R;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //We have to input data that in deck which player selected, But we have to make interface first so this time use inspector input data
+
+        King_position = transform;
+        King_vector = King_position.position;
+        test_position = 0;
+
+        s_position = new Vector3 (0f, 0f, 0f);
+
+        render_int = 0;
+
+        Health_bar = stat_ui_list[0];
+        Health_contents = stat_ui_list[1];
+
+
+    }
+
+    void Health_condition(int mode)
+    {
+        Health_bar = stat_ui_list[0];
+
+        if (mode == 0) // if mode = 0 then render Health_bar prefeb
+        {
+            GameObject render_health_bar = Instantiate(Health_bar, new Vector3(King_vector.x, King_vector.y, 0f), Quaternion.identity);
+        }
+
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        King_vector = new Vector2 (0f, 0f);
+        
+        transform.position = new Vector3(King_vector.x, King_vector.y, 0f);
+        if (render_int == 0)
+        {
+            Health_bar_R = Instantiate(Health_bar, new Vector3(King_vector.x, King_vector.y + 1.27f, 0f), Quaternion.identity);
+            Health = new GameObject("health").transform;
+            s_position = Health_bar_R.transform.position;
+
+            Debug.Log(Health_bar_R.transform.position.x);
+
+            for (int x = 0; x < 100; x++)
+            {
+                Health_contents_R = Instantiate(Health_contents, new Vector3(King_vector.x - 1.87f + (x * 0.0377f), King_vector.y, 0f), Quaternion.identity);
+                Health_contents_R.transform.SetParent(Health);
+            }
+
+
+            //Health_bar_R.transform.position = new Vector3(King_vector.x, King_vector.y + 1.27f, 0f);
+
+            render_int = 1;
+        }
+        
+
+        Debug.Log(King_vector.y - s_position[1]);
+        Health_bar_R.transform.position = new Vector3(King_vector.x, King_vector.y + 1.27f, 0f);
+        Health.transform.position = new Vector3 ( King_vector.x - s_position.x , King_vector.y - s_position.y + 2.54f);
+
+
+        //test_position += 0.001f;
+        
+    }
+}
+
---
+++ Assets/Scripts/mainsystem.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainsystem : MonoBehaviour
+{
+
+    public GameObject[] MainSystem_Ui;
+    public GameObject[] Units;
+    // Start is called before the first frame updateGam
+
+    GameObject Move_button;
+    GameObject My_king;
+    GameObject pawn;
+    GameObject Hero;
+
+    void Start()
+    {
+        My_king = Units[0];
+        pawn = Units[1];
+        Hero = Units[2];
+
+        GameObject King_R = Instantiate(My_king, new Vector3(0f, 0f, 0f), Quaternion.identity);
+        /*in this line have to spwan some pawns in board but now i have to testing about how to make main system of king the main unit,
+         so we have to add more unit but consider about hero spawn system it will be diffrent any other units*/
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        select_unit(0);
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    private void select_unit(int unit_type)
+    {
+        Vector2 mouse_pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(mouse_pos, Vector2.zero, 0f);
+        GameObject selected_unit = Units[unit_type];
+
+        if (Input.GetMouseButtonDown(0))//0 is left click 1 is right click 2 is mouse button 3
+        {
+            Collider2D col;
+
+            Debug.Log(hit.collider);
+            if (hit.collider != null)
+            {
+
+            }
+        }
+    }
+}
